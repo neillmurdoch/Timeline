ccf02f546024425c515c470b02fec2c4e4326437
Neill Murdoch <neill.murdoch@gmail.com>
2021-10-29 19:42:28 +0100

    Initial projects

---
+++ Timeline.Data/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Timeline.Data
+{
+    public class TimelineDbContext : DbContext
+    {
+        public TimelineDbContext(DbContextOptions<TimelineDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<>
+    }
+}
